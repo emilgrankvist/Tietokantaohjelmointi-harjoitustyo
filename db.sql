4c06230b4976085647330c91357b9ff7fd2bd4ea
Aktonen <aku.tolonen1999@hotmail.fi>
2022-04-28 11:40:24 +0300

    lause.

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
 create table peli (
     id int primary key auto_increment,
     peli_nimi varchar (100) not null,
-    valmistaja_id int not null,
-    genre_id int not null,
+    valmistaja_id int,
+    genre_id int,
     arvostelu_id int,
     peliaiaka_id int,
     julkaisupvm date
@@ -50,3 +50,7 @@ create table pelingenre (
     index genre_id (genre_id),
     foreign key (genre_id) references genre(id) on delete restrict
 );
+
+/*select peli_nimi, valmistaja
+from peli, valmistaja
+where peli_id = 1 and peli.id = valmistaja.peli_id*/
