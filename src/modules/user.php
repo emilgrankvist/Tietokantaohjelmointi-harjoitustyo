b9a7eedbe992bf13965e4019a4223a96c2484caa
Juhosk <n0kaju00@students.oamk.fi>
2022-05-04 16:30:16 +0300

    siirretty funktio toiseen tieodostoon ja poistettu turha

--- src/modules/user.php
+++ src/modules/user.php
@@ -8,10 +8,40 @@ function getUser(){
         // Create SQL query to get all rows from a table
         $sql = "SELECT * FROM users";
         // Execute the query
-        $people = $pdo->query($sql);
+        $user = $pdo->query($sql);
 
-        return $people->fetchAll();
+        return $user->fetchAll();
     }catch(PDOException $e){
+        throw $e;
+    }
+}
+
+function addUser($user){
+    require_once MODULES_DIR.'db.php';
+
+
+    //tarkistetaan onko muuttujia asetettu
+
+    if (!isset($user)){
+        throw new Exception("Parametreja puuttuu! Käyttäjää ei voida lisätä!");
+    }
+
+    //tarkistetaan ettei ole tyhjiä arvoja
+
+    if(empty($user)) {
+        throw new Exception("Tyhjää arvoa ei voida laittaa");
+    }
+
+    try{
+        $pdo = getPdoConnection();
+        $sql = "insert into users (username) values (?)";
+        $statement = $pdo->prepare($sql);
+        $statement->bindParam(1, $user);
+
+        $statement->execute();
+
+        echo "Käyttäjä".$user." on lisätty tietokantaan";
+    } catch (PDOException $e) {
         throw $e;
     }
 }
