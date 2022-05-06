2ffe2975730382e951ff765d187f451793406e03
Juhosk <n0kaju00@students.oamk.fi>
2022-05-06 17:25:09 +0300

    eiku nyt vasta tietokanta muutokset

--- db.sql
+++ db.sql
@@ -11,23 +11,27 @@ create table users (
 );
 drop table if exists userworkout;
 create table userworkout (
-    ID int primary key auto_increment,
+    userworkoutID int primary key auto_increment,
     usersID int not null,
     workoutdate TIMESTAMP,
     index usersID (usersID),
     FOREIGN key (usersID) REFERENCES users(ID) on delete RESTRICT
 );
+drop table if exists Exercise;
+create table Exercise (
+    ExerciseID int primary key auto_increment,
+    ExerciseType varchar(250)
+);
 drop table if exists WorkoutExercise;
 create table WorkoutExercise (
     ID int primary key not null AUTO_INCREMENT,
-    
     ExerciseID int not null,
     index ExerciseID(ExerciseID),
     foreign key (ExerciseID) REFERENCES exercise(ExerciseID),
     
     userworkoutID int not null,
     index userworkoutID(userworkoutID),
-    foreign key (ID) references userworkout(ID),
+    foreign key (userworkoutID) references userworkout(userworkoutID),
     
     
     reps int,
@@ -42,9 +46,5 @@ create table WorkoutExercise (
     reps int,
     weight int,
     primary key (exerciseID, WorkoutID)
-); /*
-drop table if exists Exercise;
-create table Exercise (
-    ExerciseID int primary key auto_increment,
-    ExerciseType varchar(250)
-);
+); */
+
